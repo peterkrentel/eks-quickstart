8ebd691cc89819b47b27e652a3ca30e4745cb942
peterkrentel <37800830+peterkrentel@users.noreply.github.com>
2025-05-29 13:12:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-eksctl create cluster -f cluster-config.yaml;
+#!/bin/bash
+#eksctl create cluster -f cluster-config.yaml;
 kubectl apply -f ingressclass.yaml;
 kubectl create namespace game-2048 --save-config;
 kubectl apply -n game-2048 -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.8.0/docs/examples/2048/2048_full.yaml;
